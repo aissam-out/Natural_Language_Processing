b39bc5e045c10ab2f9d887b7d41fb6ae966aac3e
aissam-out <aissam.outchakoucht@gmail.com>
2019-02-14 19:47:05 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,8 @@
 import os
 import re
 import nltk
-import numpy as np 
+import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+
+train = pd.read_csv("./data/labeledTrainData.tsv", header = 0, delimiter = '\t')
