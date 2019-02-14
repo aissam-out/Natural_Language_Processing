d96e5db4bba26ad34f8ba35901551f4e74e0b125
aissam-out <aissam.outchakoucht@gmail.com>
2019-02-14 19:48:43 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -4,5 +4,15 @@ import nltk
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
 
+# load the training data
 train = pd.read_csv("./data/labeledTrainData.tsv", header = 0, delimiter = '\t')
+
+def review_to_words(raw_review):
+        review_text = BeautifulSoup(raw_review, features="html5lib").get_text()
+        letters_only = re.sub("[^a-zA-Z]", " ", review_text)
+        words = letters_only.lower().split()
+        stops = set(stopwords.words("english"))
+        meaningful_words = [w for w in words if not w in stops]
+        return(" ".join( meaningful_words ))
