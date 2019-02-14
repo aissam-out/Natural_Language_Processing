ad27c69167faedd18331c88aa87bf2e70442a8aa
aissam-out <aissam.outchakoucht@gmail.com>
2019-02-14 19:54:54 +0100

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -5,10 +5,12 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 
-# load the training data
+# load the train and test data
 train = pd.read_csv("./data/labeledTrainData.tsv", header = 0, delimiter = '\t')
+test = pd.read_csv("./data/testData.tsv", header=0, delimiter="\t", quoting=3 )
 
 def review_to_words(raw_review):
         review_text = BeautifulSoup(raw_review, features="html5lib").get_text()
@@ -30,5 +32,23 @@ vectorizer = CountVectorizer(analyzer = "word",
                              stop_words = None,
                              max_features = 5000)
 
-#train_data_features = vectorizer.fit_transform(clean_train_reviews)
-#train_data_features = train_data_features.toarray()
+train_data_features = vectorizer.fit_transform(clean_train_reviews)
+train_data_features = train_data_features.toarray()
+
+forest = RandomForestClassifier(n_estimators = 150)
+forest = forest.fit( train_data_features, train["sentiment"])
+
+num_reviews = len(test["review"])
+clean_test_reviews = [] 
+
+for i in range(0,num_reviews):
+    clean_review = review_to_words( test["review"][i] )
+    clean_test_reviews.append( clean_review )
+
+test_data_features = vectorizer.transform(clean_test_reviews)
+test_data_features = test_data_features.toarray()
+
+result = forest.predict(test_data_features)
+output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+
+output.to_csv( "submit.csv", index=False, quoting=3 )
